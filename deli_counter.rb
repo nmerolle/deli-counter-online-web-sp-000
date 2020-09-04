da28ac439cabdcf32e74bbebb6fe1a22021fe190
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-04 23:05:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     line_arr = []
-    katz_deli.each_with_index
+    katz_deli.each_with_index(1) do |name, index|
+      line_arr >> 
   end
 end
