5d88df079086f14cd6bdafb12060b2872129dac8
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-04 23:55:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.length >= 1
     line_state = "The line is currently: "
     katz_deli.each_with_index do |name, index|
-      line_state += "#{index.to_i+1}. #{name}"
+      line_state += "#{index.to_i+1}. #{name} "
     end
     puts "#{line_state}"  
   else
