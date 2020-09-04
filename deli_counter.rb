b4d73f69b3a92dc05455ea96d24b3682ab9c6144
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-04 23:21:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,6 @@ def line(katz_deli)
   if katz_deli.length >= 1
     puts "The line is currently empty."
   else
-    line_arr = []
-    index = 1
-    katz_deli do |name|
-      line_arr >> "#{index}. #{index} "
-      index += 1
-    end
-    return "The line is currently: " + line_arr.join
+
   end
  end
