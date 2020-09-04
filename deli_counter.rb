00257e52a4e8941bac3e7e13f1b94f97420fcfe6
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-04 23:51:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,9 @@ def line(katz_deli)
   if katz_deli.length >= 1
     line_state = "The line is currently: "
     katz_deli.each_with_index(1) do |name, index|
-      line_arr << 
-      line_state += 
+      line_state += "#{index}. #{name}"
     end
-    
+    puts line_state  
   else
     puts "The line is currently empty."
   end
